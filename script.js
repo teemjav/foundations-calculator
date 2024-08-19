2e1b5f51234d8cf40b9b4b22782093597626ea62
teemjav <170719223+teemjav@users.noreply.github.com>
2024-08-19 10:39:49 +0100

    JS v01 complete - number functions & operators additional buttons and intricacies yet to be ironed out / complete

--- script.js
+++ script.js
@@ -14,14 +14,67 @@ const divide = function (a,b) {
     return a/b;
 };
 
-const a = document.querySelectorAll(".number");
-const operator = document.querySelector(".operator");
-const b = document.querySelectorAll(".number");
+const remainder = function (a,b) {
+    return a%b;
+}
 
-let aNumber = [];
-if (!operator) {
-    for (let i=0; i<=10; i++)
-        aNumber[i] = a.textContent;
-};
+const number = document.querySelectorAll(".number");
+const operator = document.querySelectorAll(".operator");
+const equals = document.querySelector(".equals")
 
+let operatorStore = ("");
 
+operator.forEach(function(operator) {
+    operator.addEventListener ('click', function(event) {
+        operatorStore = operator.textContent;
+        console.log(operatorStore);
+    });
+    return operatorStore;
+});
+
+let a = [];
+let b = [];
+
+number.forEach(function(number) {
+    number.addEventListener ('click', function(event) {
+        if (operatorStore === ("")) {
+            a.push(number.textContent);
+            console.log(a);
+        } else {
+            b.push(number.textContent);
+            console.log(b);
+        };
+    });
+});
+
+equals.addEventListener ('click', function(event) {
+    a = Number(a.join(''));
+    b = Number(b.join(''));
+    if (operatorStore === "+") {
+        console.log(add(a,b));
+    } else if (operatorStore === "-") {
+        console.log(subtract(a,b));
+    } else if (operatorStore === "/") {
+        console.log(divide(a,b));
+    } else if (operatorStore === "%") {
+        console.log(remainder(a,b));
+    } else if (operatorStore === "*") {
+        console.log(multiply(a,b));
+    }
+})
+
+// if (operatorStore === "") {
+//     number.forEach(function(number) {
+//         number.addEventListener ('click', function(event) {
+//             a.push(number.textContent);
+//             console.log(a);
+//         });
+//     });
+// } else {
+//     number.forEach(function(number) {
+//         number.addEventListener ('click', function(event) {
+//             b.push(number.textContent);
+//             console.log(b);
+//         });
+//     });
+// }
