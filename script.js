8f846860125ac3c9ec3d819007487cdc5905f430
teemjav <170719223+teemjav@users.noreply.github.com>
2024-08-20 22:19:41 +0100

    Publish initial release (v1.0), see JS comments

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+/*
+Things not implemented: (may be revisited in the future!)
+- X (backspace) button
+- decimal button
+- "error" when multiple operators are used (e.g. 9%/3)
+- distinction between +3 as a positive integer and +3 as a function?
+- keyboard support
+*/
+
 const add = function(a,b) {
     return a+b;
 };
@@ -21,60 +30,73 @@ const remainder = function (a,b) {
 const number = document.querySelectorAll(".number");
 const operator = document.querySelectorAll(".operator");
 const equals = document.querySelector(".equals")
+const output = document.querySelector("#output");
+const calculation = document.querySelector("#calculation")
+const clear = document.querySelector("#clear");
 
-let operatorStore = ("");
-
-operator.forEach(function(operator) {
-    operator.addEventListener ('click', function(event) {
-        operatorStore = operator.textContent;
-        console.log(operatorStore);
-    });
-    return operatorStore;
-});
-
-let a = [];
-let b = [];
+let a = [[]];
+let currentIndex = 0;
 
 number.forEach(function(number) {
     number.addEventListener ('click', function(event) {
-        if (operatorStore === ("")) {
-            a.push(number.textContent);
-            console.log(a);
-        } else {
-            b.push(number.textContent);
-            console.log(b);
-        };
+            a[currentIndex].push(number.textContent);
+            output.textContent += (number.textContent);
+            console.log(a[currentIndex]);   
     });
 });
 
-equals.addEventListener ('click', function(event) {
-    a = Number(a.join(''));
-    b = Number(b.join(''));
-    if (operatorStore === "+") {
-        console.log(add(a,b));
-    } else if (operatorStore === "-") {
-        console.log(subtract(a,b));
-    } else if (operatorStore === "/") {
-        console.log(divide(a,b));
-    } else if (operatorStore === "%") {
-        console.log(remainder(a,b));
-    } else if (operatorStore === "*") {
-        console.log(multiply(a,b));
-    }
-})
+let operatorStore = [[]];
+let operatorIndex = 0;
 
-// if (operatorStore === "") {
-//     number.forEach(function(number) {
-//         number.addEventListener ('click', function(event) {
-//             a.push(number.textContent);
-//             console.log(a);
-//         });
-//     });
-// } else {
-//     number.forEach(function(number) {
-//         number.addEventListener ('click', function(event) {
-//             b.push(number.textContent);
-//             console.log(b);
-//         });
-//     });
-// }
+operator.forEach(function(operator) {
+    operator.addEventListener ('click', function(event) {
+        operatorStore[operatorIndex] = operator.textContent;
+        output.textContent += (operator.textContent);
+        console.log(operatorStore[operatorIndex]);
+        a[currentIndex] = Number(a[currentIndex].join(''));
+        currentIndex++;
+        a.push([]);
+        operatorIndex++;
+        operatorStore.push([]);
+    });
+});
+
+
+equals.addEventListener ('click', function(event) {
+    a[currentIndex] = Number(a[currentIndex].join(''));
+    output.textContent += (equals.textContent);
+    calculation.textContent = (output.textContent);
+    output.textContent = ("");
+    result=a[0];
+    for (let i=0; i<operatorIndex; i++) {
+        if (operatorStore[i] === "+") {
+            result = add(result,a[i+1])   ;
+        } else if (operatorStore[i] === "-") {
+            result = subtract(result,a[i+1]);
+        } else if (operatorStore[i] === "/") {
+            result = divide(result,a[i+1]);
+        } else if (operatorStore[i] === "%") {
+            result = remainder(result,a[i+1]);
+        } else if (operatorStore[i] === "*") {
+            result = multiply(result,a[i+1]);
+        }
+    }
+    console.log(result);
+    output.textContent += result.toFixed(5);
+    a=[[]]; 
+    str = result.toString(); 
+    a[0] = str.split('');
+    currentIndex=0;
+    operatorStore=[];
+    operatorIndex=0;
+
+});
+
+clear.addEventListener ('click', function(event) {
+    a=[[]];
+    currentIndex=1;
+    operatorStore=[[]];
+    operatorIndex=0;
+    output.textContent="";
+    calculation.textContent="";
+});
